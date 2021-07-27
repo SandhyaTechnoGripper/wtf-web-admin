591b1da1ba25828dc9fdf0456f5dabe99395f5fe
Ankur Agarwal <agarg2311@gmail.com>
2021-07-27 23:57:27 +0530

    all API updated

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -23,7 +23,7 @@ export function Routes() {
 
     return (
         <Switch>
-            {!zisAuthorized ? (
+            {!isAuthorized ? (
                 /*Render auth page when user at `/auth` and not authorized.*/
                 <Route>
                     <AuthPage />
